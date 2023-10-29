4cdef48b741f3f8e680cac417ce0c4593f5782c3
valentina buoro <valentinabuoro4@gmail.com>
2023-10-29 20:04:47 +0100

    modal

--- Bc-FcmbHack/src/screens/Budget.jsx
+++ Bc-FcmbHack/src/screens/Budget.jsx
@@ -3,6 +3,9 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import {BiArrowBack} from 'react-icons/bi';
+import {AiOutlineClose} from 'react-icons/ai';
+import Modal from'react-modal';
+import '../styles/modal.css'
 
 const Budjet = () => {
   const [budgets, setBudgets] = useState([]);
@@ -10,7 +13,10 @@ const Budjet = () => {
   const [show, setShow] = useState(false);
 
   const showBudget = () => {
-    setShow(!show);
+    setShow(true);
+  }
+  const closeBudget = () => {
+    setShow(false);
   }
 
   const handleAddTodo = () => {
@@ -38,6 +44,7 @@ const Budjet = () => {
         ))}
       </ul>
       <div className={show? 'flex' : 'hidden'}>
+        {/*
       <div className=' w-full my-10 rounded-xl  bg-blue-100 flex items-center justify-between p-4'>
       <textarea
       className='w-full h-12 bg-inherit flex items-center justify-between border-none outline-none p-4' 
@@ -47,11 +54,27 @@ const Budjet = () => {
         onChange={(e) => setTask(e.target.value)}
       />
       <button onClick={handleAddTodo} className='py-4 px-3 bg-blue-700'>Add</button>
-      </div>
+        </div>*/}
       </div>
      <div className='flex justify-end'>
      <button onClick={showBudget} className='py-8 px-6 bg-blue-700 rounded-xl'>Create a Budget</button>
      </div>
+     <Modal isOpen={show} onClose={closeBudget} onAdd={handleAddTodo} className='p-6 h-3/6 w-10/12 mx-auto bg-white bg-opacity-100 border border-black' >
+        <div onClick={closeBudget} className='flex justify-end'>
+            <AiOutlineClose/>
+        </div>
+        
+     <div className=' w-full my-10 rounded-xl  bg-blue-100 flex items-center justify-between p-4'>
+      <textarea
+      className='w-full h-12 bg-inherit flex items-center justify-between border-none outline-none p-4' 
+        type="text"
+        placeholder="create a budget"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={handleAddTodo} className='py-4 px-3 bg-blue-700'>Add</button>
+        </div>
+     </Modal>
     </div>
   );
 };
