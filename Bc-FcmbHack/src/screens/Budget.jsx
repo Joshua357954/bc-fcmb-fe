78cee687ba85dc6efa0dc6e8a8919a5b8afa7b48
Joshua Boyi <boyijoshua72@gmail.com>
2023-10-30 12:08:16 +0100

    new

--- Bc-FcmbHack/src/screens/Budget.jsx
+++ Bc-FcmbHack/src/screens/Budget.jsx
@@ -15,37 +15,14 @@ const Budjet = () => {
   const [budgets, setBudgets] = useState([]);
   //const [task, setTask] = useState('');
   const [task, setTask] = useState({
-    title: "",
+    budgetName: "",
     amount: "",
     description: "",
-    priority_level: "",
-    label: "",
-    /*receiverBank: "",
-    accNumber: "",*/
+    priorityLevel: "",
+    receiverBank: "",
+    accNumber: "",
   });
   const [show, setShow] = useState(false);
-  const addTaskToAPI = (task) => {
-    fetch('https://bankcraft.onrender.com/api/v1/budgets', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(task),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Failed to add the task to the API');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        // Handle the response from the API if needed
-        console.log('Task added to the API:', data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
 
   const showBudget = () => {
     setShow(true);
@@ -55,14 +32,9 @@ const Budjet = () => {
   };
 
   const handleAddTodo = () => {
-    //if (task.trim() === "") return;
+    if (task.trim() === "") return;
     setBudgets([...budgets, task]);
-    setTask({ title: "", amount: "", description: "", priority_level: "", label:"", /*receiverBank: "", accNumber: ""*/ });
-    // Send the task data to the API
-    console.log(task);
-    addTaskToAPI(task);
-
-   
+    setTask("");
   };
 
   const handleDeleteTodo = (index) => {
@@ -82,43 +54,16 @@ const Budjet = () => {
       </div>
 
       <ul className="flex flex-col gap-4 p-4">
-        {budgets.map((todo, index) => (
+        {budgets.map((budget, index) => (
           <li
             key={index}
             className="flex rounded justify-between items-center px-2 py-3 mt-1 border-2 border-blue-500"
           >
-           <div>
-            <p className="font-bold text-lg">Title</p>
-           <h3>{todo.title}</h3>
-           </div>
-    <div>
-      <p className="font-bold text-base">Description</p>
-    <p>{todo.description}</p>
-    </div>
-      <div>
-        <p className="font-bold text-base">Amount</p>
-      <p>
-        {todo.amount}
-      </p>
-      </div>
-      <div>
-        <p className="font-bold text-base">Priority Level</p>
-      <p>
-        {todo.priority_level}
-      </p>
-      </div>
-     <div>
-      <p className="font-bold text-base">Label</p>
-     <p>
-        {todo.label}
-      </p>
-     </div>
-     
+            {budget}
             <button
               className="bg-red-600 px-2 py-1"
-              onClick={() => handleDeleteTodo(index)}
-            >
-              Delete
+              onClick={() => handleDeleteTodo(index)}>
+                Delete
             </button>
           </li>
         ))}
@@ -142,11 +87,7 @@ const Budjet = () => {
       <div className="flex justify-end">
         <button
           onClick={showBudget}
-<<<<<<< HEAD
           className="py-2 px-3 text-sm font-semibold bg-blue-200 rounded-lg flex items-center gap-3  "
-=======
-          className="py-4 px-6 bg-blue-700 rounded-xl"
->>>>>>> 6f1b0c93c23a53bec5488f2fafbbb3e154bf4ca4
         >
          <p> Create <br/>Budget</p> <New size={26}/> 
         </button>
@@ -155,83 +96,6 @@ const Budjet = () => {
       {/* Budget Creation */}
       <AddBudgetModal show={show} closeBudget={closeBudget} handleAddTodo={handleAddTodo} task={task} setTask={setTask} />
 
-<<<<<<< HEAD
-=======
-        <div className="flex flex-col gap-4 p-4">
-          <span className="text-xl font-semibold">Create a Budget</span>
-          <label className="text-sm font-semibold">Budget Name</label>
-          <input
-            type="text"
-            placeholder="Budget Name"
-            value={task.title}
-            onChange={(e) => setTask({ ...task, title: e.target.value })}
-            className="w-full h-12 bg-inherit flex items-center justify-between border-none outline-none p-4"
-          />
-          <label className="text-sm font-semibold">Amount</label>
-          <input
-            type="text"
-            placeholder="Amount"
-            value={task.amount}
-            onChange={(e) => setTask({ ...task, amount: e.target.value })}
-            className="w-full h-12 bg-inherit flex items-center justify-between border-none outline-none p-4"
-          />
-          <label className="text-sm font-semibold">Description</label>
-          <input
-            type="text"
-            placeholder="Description"
-            value={task.description}
-            onChange={(e) => setTask({ ...task, description: e.target.value })}
-            className="w-full h-12 bg-inherit flex items-center justify-between border-none outline-none p-4"
-          />
-          <label className="text-sm font-semibold">Priority Level</label>
-          <input
-            type="text"
-            placeholder="Priority Level"
-            value={task.priority_level}
-            onChange={(e) =>
-              setTask({ ...task, priority_level: e.target.value })
-            }
-            className="w-full h-12 bg-inherit flex items-center justify-between border-none outline-none p-4"
-          />
-           <label className="text-sm font-semibold">Label</label>
-          <input
-            type="text"
-            placeholder="Label"
-            value={task.label}
-            onChange={(e) => setTask({ ...task, label: e.target.value })}
-            className="w-full h-12 bg-inherit flex items-center justify-between border-none outline-none p-4"
-          />
-          {/*
-          <label className="text-sm font-semibold">Account Number</label>
-          <input
-            type="text"
-            placeholder="Account Number"
-            value={task.accNumber}
-            onChange={(e) => setTask({ ...task, accNumber: e.target.value })}
-            className="w-full h-12 bg-inherit flex items-center justify-between border-none outline-none p-4"
-          />
-          <label className="text-sm font-semibold">Receiver Bank</label>
-          <input
-            type="text"
-            placeholder="Reciever Bank"
-            value={task.receiverBank}
-            onChange={(e) => setTask({ ...task, receiverBank: e.target.value })}
-            className="w-full h-12 bg-inherit flex items-center justify-between border-none outline-none p-4"
-          />*/}
-
-          {/* <textarea
-          className='w-full h-12 bg-inherit flex items-center justify-between border-none outline-none p-4' 
-            type="text"
-            placeholder="create a budget"
-            value={task}
-            onChange={(e) => setTask(e.target.value)}
-      />*/}
-          <button onClick={handleAddTodo} className="py-4 px-3 bg-blue-700">
-            Add
-          </button>
-        </div>
-      </Modal>
->>>>>>> 6f1b0c93c23a53bec5488f2fafbbb3e154bf4ca4
     </div>
   );
 };
